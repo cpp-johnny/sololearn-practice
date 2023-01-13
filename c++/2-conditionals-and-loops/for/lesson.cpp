03545b610ea74c96244e2fbca99550ffd34e09b1
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2023-01-13 10:37:02 +0800

    Create lesson.cpp

---
+++ c++/2-conditionals-and-loops/for/lesson.cpp
@@ -0,0 +1,46 @@
+// normal for loop
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1;i<=5;i++) {
+        cout << i << endl;
+    }
+}
+
+// output: 1, 2, 3, 4, 5
+
+
+
+
+// for loop with `break;` --> exits the loop when condition is met
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=0;i<10;i++) {
+        if(i==5) {
+            break;
+        }
+        cout << i << endl;
+    }
+}
+
+// output: 0,1,2,3,4
+
+
+
+// for loop with `continue;` -->  The continue statement skips the current loop iteration and continues with the next one.
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=0;i<10;i++) {
+        if(i==5) {
+            continue;
+        }
+        cout << i << endl;
+    }
+}
+
+// output: 0,1,2,3,4,6,7,8,9
