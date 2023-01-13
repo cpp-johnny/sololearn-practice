3119ce69aa5b3e8f94fb3537df1ca58e7fb9a47f
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2023-01-13 12:56:25 +0800

    Create lessons.py

---
+++ python/2-interacting-with-the-user/2-working-with-Input/lessons.py
@@ -0,0 +1,14 @@
+# inputs are read asstrings
+# example: 
+
+# convert to number:
+age = int(input())
+print(age)
+
+# convert to float
+height = float(input())
+print(height) 
+
+# convert to string
+age = 42
+print("His age is " + str(age))
